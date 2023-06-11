a669fae55bf230f8f0f3853d5d9b2d682ca6adbd
TebibuS <tebibueren7@gmail.com>
2023-06-11 16:53:51 -0500

    Tebibu's Todo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,36 @@
 import React, { Component } from "react";
+import AddTodo from "C:/Users/tebib/.ssh/to-do-list_week1-TebibuS/src/component/AddTodo";
+import Todos from "C:/Users/tebib/.ssh/to-do-list_week1-TebibuS/src/component/todos";
+import "./Home.css";
+
+// Rest of the code remains the same
+
+
+
+
 
 class Home extends Component {
-  // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      // create your empty list here call it todos.
+      todos: [], // Initialize an empty array for todos
     };
   }
-  // the addTodo function simply creates a new array that includes the user submitted todo item and then
-  // updates the state with the new list.
+
   addTodo = (todo) => {
-    // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
-    // should not share duplicate values.
-    // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
-    // This solution works for a small application but a more complex hashing function should be used when
-    // dealing with a larger data sensitive project.
     todo.id = Math.random();
-    // An array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Updates the local state with the new array.
+    let newTodos = [...this.state.todos, todo];
     this.setState({
-      todos: new_list,
+      todos: newTodos,
     });
   };
+
   render() {
     return (
       <div className="Home">
-        <p> Replace this</p>
-        <h1>Todo's </h1>
+        <AddTodo addTodo={this.addTodo} />
+        <h1>Todo's</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
